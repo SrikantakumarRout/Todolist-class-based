df14e932dac45dca1a158f23c2dd2379483e5168
SrikantakumarRout <srout9333@gmail.com>
2022-10-04 11:46:42 +0530

    all

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import AddTodo from './components/AddTodo';
+import Todo from './components/Todo';
+import Todos from './components/Todos';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center">ToDo App in ReactJS</h1>
+        <Todos />
+      </div>
+    );
+  }
+}
+
+export default App;
